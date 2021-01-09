00d2026e8e09bb3de06137737623d6e270dbc2e0
linclaus <linclaus@outlook.com>
2021-01-09 16:58:47 +0800

    build

---
+++ build/ci.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ex
+
+RELEASE_IMAGE_PREFIX="linclaus/ngrok"
+NGROK_DOMAIN=$1
+
+if [ -z "$NGROK_DOMAIN" ];then
+  echo "NGROK_DOMAIN is empty"
+  return 1
+fi
+
+echo 'build and push docker image'
+docker build -t $RELEASE_IMAGE_PREFIX-$NGROK_DOMAIN . -f build/Dockerfile --build-arg NGROK_DOMAIN=$NGROK_DOMAIN \
+        && docker push $RELEASE_IMAGE_PREFIX-$NGROK_DOMAIN
